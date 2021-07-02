e4f560534c73af4713fb9a572419749fe96799b5
graywa <74241140+graywa@users.noreply.github.com>
2021-07-02 17:11:43 +0300

    some fixes

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const score = document.querySelector('.score'),
   gameArea = document.querySelector('.gameArea'),
   start = document.querySelector('.start'),
   car = document.createElement('div'),
+  btns = document.querySelectorAll('.btn'),
+  modal = document.querySelector('.modal'),
+  modalContent = document.querySelector('.modal__content'),
+  clear = document.querySelector('.btn__clear'),
   MAX_EMEMY = 7
 
 car.classList.add('car')
@@ -19,7 +23,10 @@ const settings = {
   start: false,
   score: 0,
   speed: 3,
-  traffic: 3
+  traffic: 3,
+  startSpeed: 0,
+  record: localStorage.getItem('best-record'),
+  newRecord: '',
 }
 
 function getEnemyNumber(max) {
@@ -30,14 +37,34 @@ function getCountElements (heightElement) {
   return document.documentElement.clientHeight/heightElement + 1
 }
 
-function startGame () {
-  start.classList.add('hide')
+function changeGame (lvl) {
+  switch (lvl) {
+    case '1':
+      settings.startSpeed = 3
+      settings.traffic = 3
+      break
+    case '2':
+      settings.startSpeed = 5
+      settings.traffic = 3
+      break
+    case '3':
+      settings.startSpeed = 7
+      settings.traffic = 3
+      break
+  }
+}
+
+function startGame (event) {
+  if (!event.target.classList.contains('btn')) return
+  const level = event.target.dataset.level
+  changeGame(level)
+  start.style.visibility = 'hidden'
   score.classList.remove('hide')
   gameArea.innerHTML = ''
   settings.start = true
   settings.score = 0
   car.style.top = ''
-  car.style.bottom = '10px'
+  car.style.bottom = '50px'
   car.style.left = '125px'
   for (let i = 0; i < getCountElements(40); i++) {
     const line = document.createElement('div')
@@ -82,7 +109,7 @@ function moveRoad () {
     line.y += settings.speed
     line.style.top = line.y + 'px'
 
-    if (line.y >= document.documentElement.clientHeight) line.y = - 100
+    if (line.y >= gameArea.offsetHeight) line.y = - 100
   })
 }
 
@@ -91,17 +118,27 @@ function moveEnemies () {
   enemies.forEach(item => {
     let carRect = car.getBoundingClientRect()
     let enemyRect = item.getBoundingClientRect()
-    if (carRect.right >= enemyRect.left &&
-        carRect.top <= enemyRect.bottom &&
-        carRect.left <= enemyRect.right &&
-        carRect.bottom >= enemyRect.top) {
+    if (carRect.right - 5 >= enemyRect.left &&
+        carRect.top + 5 <= enemyRect.bottom &&
+        carRect.left + 5 <= enemyRect.right &&
+        carRect.bottom - 5 >= enemyRect.top) {
           console.log('crash')
           settings.start = false
-          start.classList.remove('hide')
+          start.classList.remove('hide')        
+          modal.classList.remove('hidden')
+          score.classList.add('hide')          
+          if (settings.score > settings.record) {
+            localStorage.setItem('best-record', settings.score)            
+            settings.record = settings.score
+            settings.newRecord = settings.score
+          }          
+          modalContent.innerHTML = ` <p>speed: ${settings.speed}</p>
+                                    <p>score: ${settings.score}</p>
+                                    ${settings.newRecord && `<p><b>New best score: ${settings.newRecord}</b></p>`} `  
         }
     item.y += settings.speed - 2       
     item.style.top = item.y + 'px'
-    if (item.y > document.documentElement.clientHeight) {
+    if (item.y >= gameArea.offsetHeight) {
       item.y = -100 * settings.traffic
       item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - item.offsetWidth)) + 'px'
       item.style.background = `transparent url(./img/car${getEnemyNumber(MAX_EMEMY)}.png) no-repeat center/cover`    
@@ -114,7 +151,10 @@ function playGame () {
     moveRoad()
     moveEnemies()
     settings.score += settings.speed
-    score.innerHTML = 'score: ' + settings.score
+    score.innerHTML = `<span>speed: ${settings.speed}</span>
+                      <span> | score: ${settings.score}</span>
+                      ${settings.record ? `<span> | best score: ${settings.record}</span>` : ''}`    
+    settings.speed = settings.startSpeed + Math.floor(settings.score/2000)
     if (keys.ArrowLeft && settings.x > 0 ) {
         settings.x -= settings.speed        
       }
@@ -133,7 +173,16 @@ function playGame () {
   } 
 }
 
+function clearField () {
+  modal.classList.add('hidden')
+  gameArea.innerHTML = ''
+  score.classList.add('hide')
+  start.style.visibility = 'visible'
+}
+
 start.addEventListener('click', startGame)
+
+clear.addEventListener('click', clearField)
 
 window.addEventListener('keydown', startRun)
 
